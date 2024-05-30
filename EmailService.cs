0a7e0a4761f861a7452acac2a1f6c78d0fad8236
User <User@HOME-PC>
2024-05-30 23:15:03 +0300

    Добавьте файлы проекта.

---
+++ EmailService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_lab
+{
+    public class EmailService
+    {
+        private static readonly Lazy<EmailService> lazy = new Lazy<EmailService>(() => new EmailService());
+
+        private EmailService()
+        {
+            Console.WriteLine("Экземпляр EmailService создан.");
+        }
+        public static EmailService GetInstance()
+        {
+            return lazy.Value;
+        }
+        public void SendEmail(string to, string subject, string body)
+        {
+            Console.WriteLine($"Отправка письмо на {to} по теме \"{subject}\" и содержанием \"{body}\"");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using _6_lab;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        EmailService emailService = EmailService.GetInstance();
+        emailService.SendEmail("email@example.com", "Тест", "Текст");
+
+        EmailService emailService2 = EmailService.GetInstance();
+        emailService2.SendEmail("email2@example.com", "Тест2", "Текст2");
+    }
+}
